c8d678decc6cc9482494b333258d262b2766b102
Mark Mulder <markmulder@gmail.com>
2009-06-14 22:06:23 +0200

    Initial import

---
+++ newpost.rb
@@ -0,0 +1,22 @@
+require 'date'
+require 'fileutils'
+
+title = ARGV[0]
+file_title = title.tr(" ", "-").downcase
+
+time = DateTime::now()
+
+file_date = time.strftime("%Y-%m-%d")
+post_date = time.strftime("%B %d, %Y")
+
+post = File.new("_posts/#{file_date}-#{file_title}.textile", "w+")
+post.puts "---
+layout: post
+title: #{title}
+---
+
+h1. {{ page.title }}
+
+p(meta). #{post_date} - Amsterdam
+
+"
